a5e38408b8ead0460936543f2828ec21546b2f37
Trevor McKay <trs.mckay@gmail.com>
2020-09-25 00:20:15 -0700

    read or create config files

--- fzf-open.sh
+++ fzf-open.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
 # defaults
-OPENER="$HOME/.config/fzf-open/lopen.sh"
+LOPEN="$HOME/.config/fzf-open/lopen.sh"
+OPENER="$LOPEN"
 TERMINAL=urxvt
-STARTING_DIR="/"
+STARTING_DIR="$HOME"
 
-# source config
 CONFIG_FILE=$HOME/.config/fzf-open/config
-. $CONFIG_FILE
+if [ -f $CONFIG_FILE ]; then
+    . $CONFIG_FILE
+else
+    mkdir -p $HOME/.config/fzf-open
+    cp /usr/share/fzf-open/example_config $HOME/.config/example_config
+fi
 
-$TERMINAL --title "fzf-open-run" -e bash -c 'cd $STARTING_DIR; fzf > /tmp/fzf-open'
+if [ ! -f $LOPEN ]; then
+    cp /usr/share/fzf-open/lopen.sh $LOPEN
+fi
+
+$TERMINAL --title "fzf-open-run" -e bash -c 'cd $STARTING_DIR; fzf > /tmp/fzf-open; echo $# >> /tmp/fzf-open'
 
 if test -f "/tmp/fzf-open"; then
-    FILE=$STARTING_DIR/$(cat /tmp/fzf-open)
-    echo "$FILE"
-    $OPENER "$FILE"
+    FILE=$STARTING_DIR/$(cat /tmp/fzf-open | head -1)
+    ERROR=$(cat /tmp/fzf-open | tail -1)
+    if [ "$ERROR" == "0" ]; then
+        echo "$FILE"
+        $OPENER "$FILE"
+    fi
     rm /tmp/fzf-open
 fi
